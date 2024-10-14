ec4e61bcdcd3fb2b8c5cc767916ba75bec2bfd4b
Anshika Srivastav <150316139+anshikasrivastav1000@users.noreply.github.com>
2024-10-15 00:09:19 +0530

    mongodb

--- Downloads/journalApp (1)/journalApp/src/main/java/com/code/journalApp/Controller/JournalEntryControllerV2.java
+++ Downloads/journalApp (1)/journalApp/src/main/java/com/code/journalApp/Controller/JournalEntryControllerV2.java
@@ -1,6 +1,5 @@
 package com.code.journalApp.Controller;
 
-
 import com.code.journalApp.entity.JournalEntry;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,30 +11,28 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/_journal")
-public class JournalEntryController {
-    private Map<Long,JournalEntry> journalEntries = new HashMap<>();
+public class JournalEntryControllerV2 {
 
     @GetMapping
     public List<JournalEntry> getAll(){
-    return new ArrayList<>(journalEntries.values());
+        return null;
     }
     @PostMapping
     public boolean createEntry(@RequestBody JournalEntry myEntry){
-    journalEntries.put(myEntry.getId(),myEntry);
-    return true;
+        return true;
+
     }
     @GetMapping("id/{myId}")
     public JournalEntry getJournalEntryById(@PathVariable Long myId){
-       return journalEntries.get(myId);
+        return null;
     }
     @DeleteMapping("id/{myId}")
     public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
-        return journalEntries.remove(myId);
+
+        return null;
     }
 
     @PutMapping ("/id/{id}")
     public JournalEntry updateJournalEntryById(@PathVariable Long id,@RequestBody JournalEntry myEntry){
-        return journalEntries.put(id,myEntry);
-    }
-
-}
+        return null;
+    }};
---
+++ Downloads/journalApp (1)/journalApp/src/main/java/com/code/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,6 @@
+package com.code.journalApp.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository {
+}
